c3a3c494afb2d433afd79502eaadf005280c4bcf
DeniesKresna <batkorumbawamedsos@gmail.com>
2023-04-17 15:07:24 +0700

    add stop

--- smonitor/tracer/tracer.go
+++ smonitor/tracer/tracer.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/DeniesKresna/gobridge/serror"
 	"github.com/DeniesKresna/gohelper/utstring"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
@@ -47,9 +49,40 @@ func SetKey(span trace.Span, key string, value interface{}) {
 	span.SetAttributes(attribute.Key(key).String(valStr))
 }
 
+func SetErr(span trace.Span, err error) {
+	if err == nil {
+		return
+	}
+	span.RecordError(err)
+	span.SetStatus(codes.Error, err.Error())
+}
+
+func SetErrx(span trace.Span, errx serror.SError) {
+	if errx == nil {
+		return
+	}
+	err := errx.GetError()
+	span.RecordError(err)
+	span.SetStatus(codes.Error, err.Error())
+}
+
 func Start(ctx context.Context, spanName string) (span trace.Span, ct context.Context) {
 	spanName = utstring.CamelToSnake(spanName)
 	tr := otel.Tracer(spanName)
 	ct, span = tr.Start(ctx, spanName)
 	return
+}
+
+func Stop(span trace.Span) {
+	span.End()
+}
+
+func StopWithErr(span trace.Span, err error) {
+	SetErr(span, err)
+	span.End()
+}
+
+func StopWithErrx(span trace.Span, errx serror.SError) {
+	SetErrx(span, errx)
+	span.End()
 }
